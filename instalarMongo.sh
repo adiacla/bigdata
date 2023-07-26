934699c73060673392b377fcb8674aaae1613e88
Alfredo Antonio Diaz Claro <alfredoa.diazc@gmail.com>
2023-07-26 17:46:46 -0500

    Create instalarMongo.sh

---
+++ instalarMongo.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo Script para instalar Mongodb Alfredo Díaz en AWS EC2 Amazon Linux 2
+echo Antes de instalar verifica la version del S.O.
+grep ^NAME  /etc/*release 
+echo Si la versión no es  abstengase de intalar con este script
+echo Script para Amazon Linux
+cd /etc/yum.repos.d/ 
+sudo wget https://raw.githubusercontent.com/adiacla/bigdata/master/mongodb-org-6.0.repo
+cd ~
+sudo yum install -y mongodb-org
+echo Tipo de comando para activar el servicio de inicio
+ps --no-headers -o comm 1
+sudo systemctl start mongod
+echo Verifique que el servicio está activo
+sudo systemctl status mongod
+wget https://raw.githubusercontent.com/adiacla/bigdata/master/mongod.conf
+sudo cp mongod.conf /etc
+cd ~
+mkdir dataset
+cd dataset
+wget https://raw.githubusercontent.com/adiacla/bigdata/master/papeleria.js
+mongosh --f papeleria.js
+echo finalizar la instalacion
+exit
